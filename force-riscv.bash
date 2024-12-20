5e3aca3121c99cb57c59bea94ffda447f4ea5a34
zhaoxuying <zhaoxuying@ict.ac.cn>
2024-12-20 22:39:02 +0800

    bash:add force-riscv.bash

---
+++ force-riscv.bash
@@ -0,0 +1,8 @@
+export OBJCOPY=/nfs/home/share/riscv-toolchain-gcc15-240613/bin/riscv64-unknown-linux-gnu-objcopy
+
+export FORCE_CC=/nfs/home/zhaoxuying/.local/gcc-6.2.0/bin/g++-6.2.0
+export FORCE_PYTHON_VER=3.10
+export FORCE_PYTHON_LIB=/usr/lib/python3.10
+export FORCE_PYTHON_INC=/usr/include/python3.10/
+export PATH=/nfs/home/zhaoxuying/.local/gcc-6.2.0/bin:$PATH
+export LD_LIBRARY_PATH=/nfs/home/zhaoxuying/.local/gcc-6.2.0/lib:$LD_LIBRARY_PATH
